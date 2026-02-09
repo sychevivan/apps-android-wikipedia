765848a78990bfcfcec242f691874cb774edc566
ivan sychev <ivansychev@yandex.ru>
2026-02-09 11:42:39 +0300

    Lesson 3. Homework

---
+++ app/src/androidTest/java/org/wikipedia/lesson03/homework.kt
@@ -0,0 +1,56 @@
+package org.wikipedia.lesson03
+
+import com.google.android.material.button.MaterialButton
+import org.wikipedia.R
+
+val skipButton = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_skip_button,
+    R.string.onboarding_skip
+)
+
+val continueButton = listOf(
+    MaterialButton::class.java,
+    R.id.fragment_onboarding_forward_button,
+    R.string.onboarding_continue
+)
+
+val addLanguageButton = listOf(
+    MaterialButton::class.java,
+    R.id.addLanguageButton,
+    R.string.onboarding_multilingual_add_language_text
+)
+
+val illustrationOnboardingExploreImage = listOf(
+    MaterialButton::class.java,
+    R.id.imageViewCentered
+)
+
+val primaryTextView = listOf(
+    MaterialButton::class.java,
+    R.id.primaryTextView,
+    R.string.onboarding_welcome_title_v2
+)
+
+val secondaryTextView = listOf(
+    MaterialButton::class.java,
+    R.id.secondaryTextView,
+    R.string.onboarding_multilingual_secondary_text
+)
+
+val optionLabel = listOf(
+    MaterialButton::class.java,
+    R.id.option_label,
+)
+
+val viewOnboardingPageIndicator = listOf(
+    MaterialButton::class.java,
+    R.id.view_onboarding_page_indicator,
+)
+
+val languagesList = listOf(
+    MaterialButton::class.java,
+    R.id.languagesList,
+)
+
+
